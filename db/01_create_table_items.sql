cc71050a95fe781ecbe49697b4b951e756c9da7d
Jason Cameron <dev.jasoncameron@gmail.com>
2023-11-21 18:19:23 -0600

    itmes table creating sql file

--- db/01_create_table_items.sql
+++ db/01_create_table_items.sql
@@ -6,17 +6,19 @@ CREATE TABLE items (
                           ttrpg varchar(255),
                           rarity varchar(255),
                           renowned_quality varchar(255),
-                          req_attunement bool,
+                          requires_attunement bool,
+                          has_charges bool,
                           is_cursed bool,
                           cost double precision,
                           weight varchar(255),
                           description text,
+                          image_url varchar(255),
 
                           PRIMARY KEY (item_id)
 );
 
 ALTER SEQUENCE items_seq OWNED BY items.item_id;
 
-INSERT INTO items (name, ttrpg, rarity, renowned_quality, req_attunement, is_cursed, cost, weight, description)
+INSERT INTO items (name, ttrpg, rarity, renowned_quality, requires_attunement, has_charges, is_cursed, cost, weight, description, image_url)
 VALUES
-    ('TEST Name', 'TEST TTRPG', 'TEST RARITY', 'TEST RENOWNED QUALITY', false, false, 2.55, 'TEST WEIGHT', 'TEST DESCRIPTION');
+    ('TEST Name', 'TEST TTRPG', 'TEST RARITY', 'TEST RENOWNED QUALITY', false, false, false, 2.55, 'TEST WEIGHT', 'TEST DESCRIPTION', 'TEST IMAGE URL');

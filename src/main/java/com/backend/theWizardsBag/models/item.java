b41b4c40d33ec03831e91ef376fcd063042ef799
Jason Cameron <dev.jasoncameron@gmail.com>
2023-11-22 05:47:25 -0600

    quick add models "to rembr"

--- src/main/java/com/backend/theWizardsBag/models/item.java
+++ src/main/java/com/backend/theWizardsBag/models/item.java
@@ -3,14 +3,28 @@ package com.backend.theWizardsBag.models;
 public class item {
 
     // ATTs
-    private int item_id;
+    private int item_id;                    //   auto
     private String name;
-    private String ttrpg;
+    private String ttrpg;                   //   enum
     private String rarity;
-    private String renowned_quality;
+    private String renowned_quality;        //   enum
     private boolean requires_attunement;
     private boolean is_cursed;
     private double price;
     private String description;
     private String image_url;
+
+    // LISTs
+    /**
+     * types
+     * tags
+     * notes
+     * attached_spells
+     * effects
+     */
+
+    // OBJs
+    private String source;                  //   obj
+
+
 }
---
+++ src/main/java/com/backend/theWizardsBag/models/spell.java
@@ -0,0 +1,4 @@
+package com.backend.theWizardsBag.models;
+
+public class spell {
+}

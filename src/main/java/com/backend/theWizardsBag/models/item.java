bd73a0349f1a336f5b4b325d735c9e077de1b8c5
Jason Cameron <dev.jasoncameron@gmail.com>
2023-11-22 05:36:20 -0600

    add item to models

---
+++ src/main/java/com/backend/theWizardsBag/models/item.java
@@ -0,0 +1,16 @@
+package com.backend.theWizardsBag.models;
+
+public class item {
+
+    // ATTs
+    private int item_id;
+    private String name;
+    private String ttrpg;
+    private String rarity;
+    private String renowned_quality;
+    private boolean requires_attunement;
+    private boolean is_cursed;
+    private double price;
+    private String description;
+    private String image_url;
+}

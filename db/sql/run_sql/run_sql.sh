d2a71f64cc1954b2817fedc8133684535ceffc8a
Jason Cameron <dev.jasoncameron@gmail.com>
2023-11-25 20:14:14 -0600

    working on the bash script to automate the tedious sql files

--- db/sql/run_sql/run_sql.sh
+++ db/sql/run_sql/run_sql.sh
@@ -4,9 +4,10 @@
 FILES_DIR="/Users/jasoncameron/IdeaProjects/TheWizardsBag/db/sql/run_sql"
 
 # Loop through the files and execute them
-for FILE in "$FILES_DIR"/*.sql; do
-  echo "Running $File..."
-  psql -U postgres -d the_wizards_bag -a -f "$FILE"
-done
+for FILE in "$FILES_DIR"/*.sql;
+  do
+    echo "Running $FILE..."
+    psql -U postgres -d the_wizards_bag -a -f "$FILE"
+  done
 
 echo "Database setup complete!"

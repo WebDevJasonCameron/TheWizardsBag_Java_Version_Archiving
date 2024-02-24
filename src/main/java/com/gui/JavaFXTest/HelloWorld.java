304cb070f4ef9808609cb67cc394dc142ab452b3
Jason Cameron <dev.jasoncameron@gmail.com>
2024-02-24 09:17:57 -0600

    testing the javaFX w/n a test dir.  Will del when I understand the process

---
+++ src/main/java/com/gui/JavaFXTest/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.gui.JavaFXTest;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}

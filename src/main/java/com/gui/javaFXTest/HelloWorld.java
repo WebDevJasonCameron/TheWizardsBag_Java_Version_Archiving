685fa0ba8a20a7d00a56893d1e9d588b8c0c4cff
Jason Cameron <dev.jasoncameron@gmail.com>
2024-02-26 05:38:13 -0600

    cont learning jfx

--- src/main/java/com/gui/javaFXTest/HelloWorld.java
+++ src/main/java/com/gui/javaFXTest/HelloWorld.java
@@ -1,18 +1,34 @@
 package com.gui.javaFXTest;
 
 import javafx.application.Application;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class HelloWorld extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Hello World");
-        System.out.println("hello World");
+        primaryStage.setTitle("Test");
+
+        primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            System.out.println("Key pressed: " + event.toString());
+
+            switch (event.getCode().getCode()){
+                case 27 : { // 27 = ESC key
+                    primaryStage.close();
+                    break;
+                }
+                case 10 : { // 10 = Return
+                    primaryStage.setWidth( primaryStage.getWidth() * 2);
+                }
+                default: {
+                    System.out.println("Unrecognized Key");
+                }
+            }
+        });
+
 
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
+
 }

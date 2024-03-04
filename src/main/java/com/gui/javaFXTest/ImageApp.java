78e503d5cc7f877df0209befcab3f54f3fba1c74
Jason Cameron <dev.jasoncameron@gmail.com>
2024-03-04 05:48:44 -0600

    build image app

---
+++ src/main/java/com/gui/javaFXTest/ImageApp.java
@@ -0,0 +1,23 @@
+package com.gui.javaFXTest;
+
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class ImageApp extends Application {
+
+    // ATTs
+    private final static String IMAGE_LOC = "images/MoonWave.png";
+
+    // CONs
+
+    // MTHs
+
+    // OVRs
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Image image2 = new Image(IMAGE_LOC, 360.0d, 360.0d, true, true);
+        Image image3 = new Image(IMAGE_LOC, 360.0d, 360.0d, false, true);
+        Image image4 = new Image(IMAGE_LOC);
+    }
+}

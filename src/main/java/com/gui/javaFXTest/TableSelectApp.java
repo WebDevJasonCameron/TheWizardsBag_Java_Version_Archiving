29000745a1aef909588857e3b225e796de68275a
Jason Cameron <dev.jasoncameron@gmail.com>
2024-03-03 18:56:35 -0600

    table made.  learn how next

--- src/main/java/com/gui/javaFXTest/TableSelectApp.java
+++ src/main/java/com/gui/javaFXTest/TableSelectApp.java
@@ -1,10 +1,15 @@
 package com.gui.javaFXTest;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -40,7 +45,47 @@ public class TableSelectApp extends Application {
                 colSKU, colDesc, colPrice, colTaxable
         );
 
+        tblItems.getItems().addAll(
+                new Item("Kbd-0455892", "Mechanical Keyboard", 100.0f, true),
+                new Item("145266", "Product Docs", 0.0f, false),
+                new Item("OR-198975", "O-Ring (100)", 10.0f, true)
+        );
 
+        Button btnInventory = new Button("Inventory");
+        Button btnCalcTax = new Button("Tax");
+
+        btnInventory.disableProperty().bind(
+                tblItems.getSelectionModel().selectedItemProperty().isNull() );
+
+        btnCalcTax.disableProperty().bind(
+                tblItems.getSelectionModel().selectedItemProperty().isNull().or(
+                        Bindings.select(
+                                tblItems.getSelectionModel().selectedItemProperty(),
+                                "taxable"
+                        ).isEqualTo(false)
+                )
+        );
+
+        HBox buttonHBox = new HBox( btnInventory, btnCalcTax );
+        buttonHBox.setSpacing( 8 );
+
+        VBox vBox = new VBox( tblItems, buttonHBox );
+        vBox.setPadding( new Insets(10) );
+        vBox.setSpacing( 10 );
+
+        Scene scene = new Scene(vBox);
+
+        primaryStage.setTitle( "TableSelectApp" );
+        primaryStage.setScene( scene );
+        primaryStage.setHeight( 367 );
+        primaryStage.setWidth( 667 );
+        primaryStage.show();
+
+    }
+
+    // RUN
+    public static void main(String[] args) {
+        launch(args);
     }
 }
 

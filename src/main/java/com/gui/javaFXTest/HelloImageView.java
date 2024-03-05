52a7dd02a0424808eab454fb225b201b565472eb
Jason Cameron <dev.jasoncameron@gmail.com>
2024-03-05 06:22:00 -0600

    works but doesnt work. get black or white screen, no image

--- src/main/java/com/gui/javaFXTest/HelloImageView.java
+++ src/main/java/com/gui/javaFXTest/HelloImageView.java
@@ -10,40 +10,47 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class HelloImageView extends Application {
+
+    private final static String IMAGE_LOC = "images/avatar_0001.png";
+
     @Override public void start(Stage stage) {
         // load the image
-        Image image = new Image("MoonWave.png");
+        Image image = new Image(new File(IMAGE_LOC).toURI().toString());
 
         // simple displays ImageView the image as is
         ImageView iv1 = new ImageView();
         iv1.setImage(image);
+        iv1.setLayoutX(800);
+        iv1.setLayoutY(400);
 
         // resizes the image to have width of 100 while preserving the ratio and using
         // higher quality filtering method; this ImageView is also cached to
         // improve performance
-        ImageView iv2 = new ImageView();
-        iv2.setImage(image);
-        iv2.setFitWidth(100);
-        iv2.setPreserveRatio(true);
-        iv2.setSmooth(true);
-        iv2.setCache(true);
+        //ImageView iv2 = new ImageView();
+        //iv2.setImage(image);
+        //iv2.setFitWidth(100);
+        //iv2.setPreserveRatio(true);
+        //iv2.setSmooth(true);
+        //iv2.setCache(true);
 
         // defines a viewport into the source image (achieving a "zoom" effect) and
         // displays it rotated
-        ImageView iv3 = new ImageView();
-        iv3.setImage(image);
-        Rectangle2D viewportRect = new Rectangle2D(40, 35, 110, 110);
-        iv3.setViewport(viewportRect);
-        iv3.setRotate(90);
+//        ImageView iv3 = new ImageView();
+//        iv3.setImage(image);
+//        Rectangle2D viewportRect = new Rectangle2D(40, 35, 110, 110);
+//        iv3.setViewport(viewportRect);
+//        iv3.setRotate(90);
 
         Group root = new Group();
         Scene scene = new Scene(root);
-        scene.setFill(Color.BLACK);
+        //scene.setFill(Color.BLACK);
         HBox box = new HBox();
         box.getChildren().add(iv1);
-        box.getChildren().add(iv2);
-        box.getChildren().add(iv3);
+//        box.getChildren().add(iv2);
+//        box.getChildren().add(iv3);
         root.getChildren().add(box);
 
         stage.setTitle("ImageView");

5290f8f83ad205c7a50354a9dd326312e809fa90
Jason Cameron <dev.jasoncameron@gmail.com>
2024-09-02 20:13:29 -0500

    still fig out the input valls

--- src/main/java/com/cli/theWizardsBag/MenuCons/NumericInputHandler.java
+++ src/main/java/com/cli/theWizardsBag/MenuCons/NumericInputHandler.java
@@ -6,10 +6,13 @@ public class NumericInputHandler extends ValidationInputHandler {
     @Override
     protected boolean isValid(String input) {
         // Example validation logic: check if the input is a numeric value
+
         try {
             Integer.parseInt(input);
+            System.out.println("yup here");
             return true;
         } catch (NumberFormatException e) {
+            System.out.println("nope here");
             return false;
         }
     }
--- src/main/java/com/cli/theWizardsBag/Menus/MainMenu.java
+++ src/main/java/com/cli/theWizardsBag/Menus/MainMenu.java
@@ -12,35 +12,35 @@ public class MainMenu extends Menu {
 
         addOption(new MenuOption("Add Item", () -> {
             System.out.println("Add Magical Item...");
-        }, new NumericInputHandler()));
+        }));
 
         addOption(new MenuOption("Edit Item", () -> {
             System.out.println("Edit Magical Item...");
-        }, new NumericInputHandler()));
+        }));
 
         addOption(new MenuOption("View Item", () -> {
             System.out.println("View Magical Item...");
-        }, new NumericInputHandler()));
+        }));
 
         addOption(new MenuOption("Delete Item", () -> {
             System.out.println("Delete Magical Item...");
-        }, new NumericInputHandler()));
+        }));
 
         addOption(new MenuOption("Add Spell", () -> {
             System.out.println("Add Magical Spell...");
-        }, new NumericInputHandler()));
+        }));
 
         addOption(new MenuOption("Edit Spell", () -> {
             System.out.println("Edit Magical Spell...");
-        }, new NumericInputHandler()));
+        }));
 
         addOption(new MenuOption("View Spell", () -> {
             System.out.println("View Magical Spell...");
-        }, new NumericInputHandler()));
+        }));
 
         addOption(new MenuOption("Delete Spell", () -> {
             System.out.println("Delete Magical Spell...");
-        }, new NumericInputHandler()));
+        }));
     }
 
     // OVRs

80ba60ba3e8f741f020352ce3c99f238b14ab099
Jason Cameron <dev.jasoncameron@gmail.com>
2024-10-05 15:31:29 -0500

    working on the tags menu

--- src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
+++ src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
@@ -1,21 +1,40 @@
 package com.cli.theWizardsBag.Menus;
 
+import com.backend.theWizardsBag.models.Tag;
+import com.backend.theWizardsBag.utils.Executables.TagJDBCExecutor;
 import com.cli.theWizardsBag.MenuCons.Menu;
+import com.cli.theWizardsBag.MenuCons.MenuOption;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class ViewSpellByTagMenu extends Menu {
 
     Scanner scanner = new Scanner(System.in);
+    TagJDBCExecutor tagJDBCExecutor = new TagJDBCExecutor();
+    List<Tag> tags = tagJDBCExecutor.getAllByTagTypes("spell", "both");
 
     // CONs
     public ViewSpellByTagMenu(String title) {
-        super(title);
+        super("View Spell by Its Tag Menu");
+
+        for (Tag tag : tags) {
+            addOption(new MenuOption(tag.getTagName(), this::viewByTag));
+        }
     }
+
+
+    // METHs
+    private void viewByTag() {}
+
 
     // OVRs
     @Override
     protected void handleUserChoice(int choice) {
-
+        if (choice > 0 && choice <= options.size()) {
+            options.get(choice - 1).execute();
+        } else {
+            System.out.println("Invalid choice, please try again");
+        }
     }
 }

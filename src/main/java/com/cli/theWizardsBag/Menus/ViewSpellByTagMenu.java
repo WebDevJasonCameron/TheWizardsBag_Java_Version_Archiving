4dd1be2b69a0bad2a0adafd4a4898e1f45f04314
Jason Cameron <dev.jasoncameron@gmail.com>
2024-10-07 05:49:33 -0500

    I think I am creating a new menu "list" sub class soon

--- src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
+++ src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
@@ -1,45 +1,28 @@
 package com.cli.theWizardsBag.Menus;
 
 import com.backend.theWizardsBag.models.Tag;
-import com.backend.theWizardsBag.utils.Executables.SpellTagJDBCExecutor;
 import com.backend.theWizardsBag.utils.Executables.TagJDBCExecutor;
-import com.cli.theWizardsBag.MenuCons.Menu;
-import com.cli.theWizardsBag.MenuCons.MenuOption;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ViewSpellByTagMenu extends Menu {
+public class ViewSpellByTagMenu {
 
     Scanner scanner = new Scanner(System.in);
     TagJDBCExecutor tagJDBCExecutor = new TagJDBCExecutor();
     List<Tag> tags = tagJDBCExecutor.getAllByTagTypes("spell", "both");
+    // create new object with list number, tag name, and tag id
 
     // CONs
-    public ViewSpellByTagMenu(String title) {
-        super("View Spell by Its Tag Menu");
 
-    }
 
 
     // METHs
-    private List<MenuOption> viewByTag() {
-        SpellTagJDBCExecutor spellTagJDBCExecutor = new SpellTagJDBCExecutor();
-        List<MenuOption> options = new ArrayList<>();
-        for (Tag tag : tags) {
-            options.add(new MenuOption(tag.getTagName(), spellTagJDBCExecutor.getAllByTagId(tag.getTagId()));
-        }
-    }
+    // List tags to select
+    // Number selected cause the "get all spells with tag id"
 
 
-    // OVRs
-    @Override
-    protected void handleUserChoice(int choice) {
-        if (choice > 0 && choice <= options.size()) {
-            options.get(choice - 1).execute();
-        } else {
-            System.out.println("Invalid choice, please try again");
-        }
-    }
+
+
+
 }

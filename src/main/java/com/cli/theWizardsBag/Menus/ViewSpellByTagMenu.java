b7b928bb9a732dd5fed5d8c937e35f007763b8e2
Jason Cameron <dev.jasoncameron@gmail.com>
2024-10-08 05:56:07 -0500

    trying to add args to the menuOption so we can auto manu the list search

--- src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
+++ src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
@@ -11,6 +11,7 @@ public class ViewSpellByTagMenu {
     Scanner scanner = new Scanner(System.in);
     TagJDBCExecutor tagJDBCExecutor = new TagJDBCExecutor();
     List<Tag> tags = tagJDBCExecutor.getAllByTagTypes("spell", "both");
+
     // create new object with list number, tag name, and tag id
 
     // CONs

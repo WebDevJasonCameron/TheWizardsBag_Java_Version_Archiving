d027100121d2d0423d6f0d9240c23a218d370eb0
Jason Cameron <dev.jasoncameron@gmail.com>
2024-10-09 06:03:19 -0500

    forgot lambda is a thing!  Thank you jipeety

--- src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
+++ src/main/java/com/cli/theWizardsBag/Menus/ViewSpellByTagMenu.java
@@ -1,28 +1,46 @@
 package com.cli.theWizardsBag.Menus;
 
 import com.backend.theWizardsBag.models.Tag;
+import com.backend.theWizardsBag.utils.Executables.SpellTagJDBCExecutor;
 import com.backend.theWizardsBag.utils.Executables.TagJDBCExecutor;
+import com.cli.theWizardsBag.MenuCons.Menu;
+import com.cli.theWizardsBag.MenuCons.MenuOption;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ViewSpellByTagMenu {
+public class ViewSpellByTagMenu extends Menu {
 
     Scanner scanner = new Scanner(System.in);
     TagJDBCExecutor tagJDBCExecutor = new TagJDBCExecutor();
     List<Tag> tags = tagJDBCExecutor.getAllByTagTypes("spell", "both");
-
-    // create new object with list number, tag name, and tag id
+    List<MenuOption> menuOptions = getMenuOptions(tags);
 
     // CONs
-
-
+    public ViewSpellByTagMenu(String title) {
+        super(title);
+        menuOptions.addAll(getMenuOptions(tags));
+    }
 
     // METHs
-    // List tags to select
-    // Number selected cause the "get all spells with tag id"
+    private List<MenuOption> getMenuOptions(List<Tag> tags) {
+        SpellTagJDBCExecutor spellTagJDBCExecutor = new SpellTagJDBCExecutor();
+        List<MenuOption> menuOptionsOutput = new ArrayList<>();
+        for (Tag tag : tags) {
+            menuOptionsOutput.add(new MenuOption(tag.getTagName(), () -> {spellTagJDBCExecutor.getAllByTagId(tag.getTagId());
+            }));
+        }
+
+        return menuOptionsOutput;
+    }
 
 
+    // OVRs
+    @Override
+    protected void handleUserChoice(int choice) {
+
+    }
 
 
 

1829bbbc8acccab4c86abb94d28298a6b84f359e
Jason Cameron <dev.jasoncameron@gmail.com>
2024-12-11 18:41:33 -0600

    visual component

--- src/main/java/com/cli/theWizardsBag/Menus/CreateSpellMenu.java
+++ src/main/java/com/cli/theWizardsBag/Menus/CreateSpellMenu.java
@@ -156,13 +156,14 @@ public class CreateSpellMenu extends Menu {
     private boolean isSpellComponentVisual() {
         System.out.println("Is there a visual component to the spell? ");
 
-        Map<String, String> spellComponentBinary = new HashMap<>();
-        String spellComponentVisualOutput = "";
-        int count = 1;
-        spellComponentBinary.put(String.valueOf(count), "false");
-        spellComponentBinary.put(String.valueOf(count), "true");
+        Map<String, Boolean> spellComponentBinary = new HashMap<>();
+        boolean spellComponentVisualOutput = false;
 
-        // <!> Loop through
+        int count = 1;
+        spellComponentBinary.put(String.valueOf(count), false);
+        spellComponentBinary.put(String.valueOf(count), true);
+
+        // <!> not sure, but list out
 
 
         return scanner.nextBoolean();
